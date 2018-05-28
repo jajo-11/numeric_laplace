f2b9c48f4d0b60a27d1a8d2aef3f8df248aca37c
jajo-11 <jajo_11@outlook.com>
2018-05-28 21:49:58 +0200

    fixed formatting

--- src/plot.rs
+++ src/plot.rs
@@ -5,14 +5,14 @@ use std::io::Write;
 pub fn plot_2d_color_map(file: &str) {
     let mut temp = File::create("temp").expect("Could not create temp fileS");
     let file_string = format!(
-        " set terminal pngcairo  transparent enhanced font \"arial,10\" fontscale 1.0 size 600, 400\
-            \nset output 'out.png'
-            \nset title \"Electrical Potential Approximation Based On Laplace Equation\"\
-            \nset cblabel \"Potential in kV\"\
-            \nset datafile separator comma
-            \nset autoscale xfix
-            \nset autoscale yfix
-            \nplot \"{}\" matrix nonuniform using 1:2:3 with image", file);
+        "set terminal pngcairo  transparent enhanced font \"arial,10\" fontscale 1.0 size 600, 400
+set output 'out.png'
+set title \"Electrical Potential Approximation Based On Laplace Equation\"
+set cblabel \"Potential in kV\"
+set datafile separator comma
+set autoscale xfix
+set autoscale yfix
+plot \"{}\" matrix nonuniform using 1:2:3 with image", file);
     temp.write_all(file_string.as_bytes()).expect("Could not write to temp file");
     Command::new("gnuplot")
         .args(&["-p", "temp"])
